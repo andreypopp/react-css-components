dd3e8d588d285fd356b736c6615026e0852ac61d
Andrey Popp <8mayday@gmail.com>
2016-04-10 00:10:21 +0300

    feat: add list of supported pseudo classes

--- src/transformVariants.js
+++ src/transformVariants.js
@@ -3,6 +3,36 @@
  * @flow
  */
 
+/**
+ * Variant names we want to see compiled as CSS pseudo classes.
+ */
+const SUPPORTED_PSEUDO_CLASSES = {
+  focus: true,
+  hover: true,
+  active: true,
+  checked: true,
+  default: true,
+  disabled: true,
+  empty: true,
+  enabled: true,
+  firstChild: true,
+  fullscreen: true,
+  indeterminate: true,
+  invalid: true,
+  lastChild: true,
+  left: true,
+  link: true,
+  onlyChild: true,
+  optional: true,
+  required: true,
+  right: true,
+  root: true,
+  scope: true,
+  target: true,
+  valid: true,
+  visited: true,
+};
+
 export default function transformVariants(root) {
   let toAppend = [];
   root.each(rule => {
