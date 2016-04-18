7d177461e42c4ae5b94de6766a7201e8507aa9de
Andrey Popp <8mayday@gmail.com>
2016-04-18 22:30:33 +0300

    test: add case for custom variant with pseudo

---
+++ src/__tests__/fixtures/custom-variant-pseudo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "css";
+export function Label({
+  variant,
+  ...props }) {
+  let className = styles.Label + (variant.custom ? ' ' + styles.Label__custom : '');
+  return React.createElement("div", { ...props, className
+  });
+}
